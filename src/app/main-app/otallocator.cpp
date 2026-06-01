0d0224090d1bb511e75ebc40a0318fa673a9ccb1
Z1nK <llobesa@gmail.com>
2026-06-01 17:03:59 +0200

    Add main otallocator application

---
+++ src/app/main-app/otallocator.cpp
@@ -0,0 +1,26 @@
+#include <array>
+#include <iostream>
+#include <array>
+
+#include <map>
+
+#include <vector>
+
+#include <allocators/log_alloc.hpp>
+#include <allocators/arena_alloc.hpp>
+
+int factorial(int n) {
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
+}
+
+int main() {
+
+  std::map<int, int> standart_map;
+  auto hint = standart_map.end();
+  for (int i = 0; i <= 9; ++i) {    
+    hint = standart_map.insert(hint, {i, factorial(i)});
+  }
+    
+  return 0;
+}
