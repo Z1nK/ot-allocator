db4287ad9c81016c888aa9ad84bdcb8cb419bb79
Z1nK <llobesa@gmail.com>
2026-06-02 15:28:20 +0200

    Update main executable

--- src/app/main-app/otallocator.cpp
+++ src/app/main-app/otallocator.cpp
@@ -1,27 +1,52 @@
 #include <array>
 #include <iostream>
-#include <array>
 
 #include <map>
 
 #include <vector>
 
-#include <allocators/log_alloc.hpp>
 #include <allocators/arena_alloc.hpp>
 #include <allocators/heap_arena_alloc.hpp>
+#include <allocators/log_alloc.hpp>
+
+constexpr std::size_t MAP_ARENA_SIZE = 512;
 
 int factorial(int n) {
-  if (n <= 1) return 1;
+  if (n <= 1)
+    return 1;
   return n * factorial(n - 1);
 }
 
 int main() {
 
+  using MapAlloc = ArenaAllocator<std::pair<const int, int>, MAP_ARENA_SIZE>;
+
+  // 1. creating an instance of std::map<int, int> with standart allocator
   std::map<int, int> standart_map;
   auto hint = standart_map.end();
-  for (int i = 0; i <= 9; ++i) {    
+  for (int i = 0; i <= 9; ++i) {
     hint = standart_map.insert(hint, {i, factorial(i)});
   }
-    
+
+  for (const auto &[key, value] : standart_map) {
+    std::cout << key << " " << value << "\n";
+  }
+  // 2. creating an instance of std::map<int, int> with custom allocator (arena
+  // allocator on stack)
+
+  MemoryArena<MAP_ARENA_SIZE> map_arena;
+  MapAlloc alloc(map_arena);
+  std::map<int, int, std::less<int>, MapAlloc> stack_map(std::less<int>(),
+                                                         alloc);
+
+  hint = stack_map.end();
+  for (int i = 0; i <= 9; ++i) {
+    hint = stack_map.insert(hint, {i, factorial(i)});
+  }
+
+  for (const auto &[key, value] : stack_map) {
+    std::cout << key << " " << value << "\n";
+  }
+
   return 0;
 }
